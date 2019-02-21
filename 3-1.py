3e35fd4b62e656f9ffdee505fd34e89e560f1cdb
Han0509 <https://github.com/Han0509/GitTest.git>
2019-02-21 20:15:47 +0800

    3-1

---
+++ 3-1.py
@@ -0,0 +1,49 @@
+'mathScore = [60, 100, 20, 2, 8]'
+'print(mathScore[2:4])'
+'print(mathScore[-2])'
+'print(mathScore[-2:])'
+'print(mathScore[:4])'
+'print(len(mathScore))'
+'x = sum(mathScore)'
+'y = len(mathScore)'
+'print(x/y)'
+#-----------------
+'z = 100'
+'q = 20'
+'z = int(z/q)'
+'print(z)'
+#-----------------
+'ls =[2,8,9]'
+'ls.append(4)#尾巴'
+'print(ls)'
+'ls.insert(0,5)#從位置0的後面插入5'
+'ls.insert(3,20)#從位置3的後面插入20'
+'print(ls)'
+#---------------------------------------------
+'del mathScore[2:4]#前一位算法為01234:後一位算法為1234'
+'print(mathScore)'
+'mathScore.remove(20)'
+'print(mathScore)'
+
+#ls = ['a' ,'c']
+'print(mathScore + ls)'
+#-----------------------------------------------------
+'ls =[[1,3],[6,5]]'
+'print(ls[0][0])#第一個零是讀外層/第二個零是讀外層'
+'print(ls[0][1])'
+'print(ls[1][0])'
+'print(ls[1][1])'
+#----------------------------------------------
+'print(sorted(mathScore))'
+grades = [[5,14,7],[23,36,28],[88,80,92]]
+print(grades[2])
+x = sum(grades[0])
+print(x/3)
+y = sum(grades[1])
+print(y/len(grades))
+z = sum(grades[2])
+print(z/3)
+grades.append([94,90,96])
+print(grades)
+
+'練習時請刪掉單引號及井字號'
